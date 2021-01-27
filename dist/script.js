2c245db7f5545ba60ef2e4c83e1f4049d31b0d61
Chance Whitaker <chancellor_whitak21@mymail.eku.edu>
2021-01-27 14:41:04 -0500

    initializing repository

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+function hoverOn(img) {
+	img.src = 'https://upload.wikimedia.org/wikipedia/commons/7/71/Choloepus_hoffmanni_%28Puerto_Viejo%2C_CR%29_crop.jpg';
+}
+
+function hoverOff(img) {
+	img.src = 'https://upload.wikimedia.org/wikipedia/commons/b/be/Bicho-pregui%C3%A7a_3.jpg';
+}
